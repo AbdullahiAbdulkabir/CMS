2ca79ddff4abea1d49554eae92f4c7ceb4b834a2
AbdullahiAbdulkabir <33360580+AbdullahiAbdulkabir@users.noreply.github.com>
2017-12-19 21:41:44 +0100

    Delete connect.php

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php 
-// 1. CREATE A DATABASE CONNECTION
-$connection = mysql_connect('localhost', 'root', ''); 
-// 2. SELECT A DATABSE TO USE
-$db_select = mysql_select_db("widget_corp", $connection);
-
- ?>
